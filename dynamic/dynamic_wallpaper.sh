14c23d1a4eea155cf88faeb11b7fafbfead2ec8b
Tom Kamoss <tom.kamoss@web.de>
2023-04-11 18:23:27 +0200

    added wallpapers and dynamic La-Saline slideshow

---
+++ dynamic/dynamic_wallpaper.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# get current location
+location=$(curl -s https://ipinfo.io/loc)
+
+# get timezone
+timezone=$(curl -s https://ipinfo.io/timezone)
+
+# calculate sunrise and sunset times
+sunrise=$(curl -s "https://api.sunrise-sunset.org/json?lat=${location%,*}&lng=${location#*,}&formatted=0" | jq -r '.results.sunrise')
+sunset=$(curl -s "https://api.sunrise-sunset.org/json?lat=${location%,*}&lng=${location#*,}&formatted=0" | jq -r '.results.sunset')
+sunrise_unix=$(TZ=$timezone date -d "$sunrise" +%s)
+sunset_unix=$(TZ=$timezone date -d "$sunset" +%s)
+
+# calculate one hour before and after sunrise and sunset
+one_hour=3600
+sunrise_before_unix=$((sunrise_unix-one_hour))
+sunrise_after_unix=$((sunrise_unix+one_hour))
+sunset_before_unix=$((sunset_unix-one_hour))
+sunset_after_unix=$((sunset_unix+one_hour))
+
+# update XML file
+xmlstarlet edit --inplace \
+  --update '/background/starttime/year' --value "$(TZ=$timezone date -d @$sunrise_unix +'%Y')" \
+  --update '/background/starttime/month' --value "$(TZ=$timezone date -d @$sunrise_unix +'%-m')" \
+  --update '/background/starttime/day' --value "$(TZ=$timezone date -d @$sunrise_unix +'%-d')" \
+  --update '/background/starttime/hour' --value "$(TZ=$timezone date -d @$sunrise_after_unix +'%-H')" \
+  --update '/background/starttime/minute' --value "$(TZ=$timezone date -d @$sunrise_after_unix +'%-M')" \
+  --update '/background/starttime/second' --value "$(TZ=$timezone date -d @$sunrise_after_unix +'%-S')" \
+  --update '/background/static[1]/duration' --value "$((sunset_before_unix-sunrise_after_unix))" \
+  --update '/background/static[2]/duration' --value "$((sunrise_before_unix+86400-sunset_after_unix))" \
+ /path/to/La-Saline.xml
+
